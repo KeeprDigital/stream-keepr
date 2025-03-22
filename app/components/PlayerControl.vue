f7c1810090341616adbce9b7f5d96cdb32c3ae02
Marcus Mailer <marcus@keepr.digital>
2025-03-22 15:36:33 +1100

    feat: player controls title

--- app/components/PlayerControl.vue
+++ app/components/PlayerControl.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+type Props = {
+  title?: string
+}
+
+const props = defineProps<Props>()
 
 const state = defineModel<PlayerData>(
   {
@@ -17,35 +22,41 @@ const state = defineModel<PlayerData>(
 </script>
 
 <template>
-  <UForm class="player-control" :state="state">
-    <UFormField label="Name" name="name">
-      <UInput v-model="state.name" class="input" />
-    </UFormField>
-    <UFormField label="Pronouns" name="proNouns">
-      <UInput v-model="state.proNouns" class="input" />
-    </UFormField>
-    <UFormField label="Deck" name="deck">
-      <UInput v-model="state.deck" class="input" />
-    </UFormField>
-    <UFormField label="Position" name="position">
-      <UInput v-model="state.position" class="input" />
-    </UFormField>
-    <div class="score">
-      <UFormField class="score-field" label="Wins" name="wins">
-        <UInput v-model="state.score.wins" class="input" />
+  <section class="player-control">
+    <h3 v-if="props.title" class="text-center">
+      {{ props.title }}
+    </h3>
+    <UForm class="player-control-form" :state="state">
+      <UFormField label="Name" name="name">
+        <UInput v-model="state.name" class="input" />
       </UFormField>
-      <UFormField class="score-field" label="Losses" name="losses">
-        <UInput v-model="state.score.losses" class="input" />
+      <UFormField label="Pronouns" name="proNouns">
+        <UInput v-model="state.proNouns" class="input" />
       </UFormField>
-      <UFormField class="score-field" label="Draws" name="draws">
-        <UInput v-model="state.score.draws" class="input" />
+      <UFormField label="Deck" name="deck">
+        <UInput v-model="state.deck" class="input" />
       </UFormField>
-    </div>
-  </UForm>
+      <div class="score">
+        <UFormField class="score-field" label="Wins" name="wins">
+          <UInput v-model="state.score.wins" class="input" />
+        </UFormField>
+        <UFormField class="score-field" label="Losses" name="losses">
+          <UInput v-model="state.score.losses" class="input" />
+        </UFormField>
+        <UFormField class="score-field" label="Draws" name="draws">
+          <UInput v-model="state.score.draws" class="input" />
+        </UFormField>
+      </div>
+      <UFormField label="Position" name="position">
+        <UInput v-model="state.position" class="input" />
+      </UFormField>
+    </UForm>
+  </section>
 </template>
 
 <style scoped>
-.player-control {
+.player-control,
+.player-control-form {
   display: flex;
   flex-direction: column;
   gap: 1rem;
