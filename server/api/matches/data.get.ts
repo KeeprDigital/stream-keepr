d19d3f117b508a6b3241325ae039a68e6cf64be2
marcus-mailer <marcus.mailer@zerocap.com>
2025-05-07 22:00:10 +1000

    fix: player score

--- server/api/matches/data.get.ts
+++ server/api/matches/data.get.ts
@@ -4,5 +4,16 @@ export default defineEventHandler(async () => {
   const local = useStorage('local')
   const matchData = await local.getItem<MatchDataList>('matches')
 
+  if (matchData) {
+    matchData.forEach((match) => {
+      if (!match.playerOne.position) {
+        match.playerOne.position = `${match.playerOne.score.wins}-${match.playerOne.score.losses}${match.playerOne.score.draws > 0 ? `-${match.playerOne.score.draws}` : ''}`
+      }
+      if (!match.playerTwo.position) {
+        match.playerTwo.position = `${match.playerTwo.score.wins}-${match.playerTwo.score.losses}${match.playerTwo.score.draws > 0 ? `-${match.playerTwo.score.draws}` : ''}`
+      }
+    })
+  }
+
   return matchData
 })
