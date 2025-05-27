e1c42ba0298ede723cccedd4375b93b22f601e9c
marcus-mailer <marcus.mailer@zerocap.com>
2025-05-27 13:34:46 +1000

    feat: types clean up and mapped ack type

---
+++ shared/types/ack.ts
@@ -0,0 +1,24 @@
+export type BaseAckResponse = {
+  success: boolean
+  timestamp?: string
+}
+
+export type SuccessAckResponse = {
+  success: true
+} & BaseAckResponse
+
+export type ErrorAckResponse = {
+  success: false
+  error: string
+  code?: string
+  details?: any
+} & BaseAckResponse
+
+export type AckResponse = SuccessAckResponse | ErrorAckResponse
+export type AckCallback = (response: AckResponse) => void
+
+export type AddAckToActions<T> = {
+  [K in keyof T]: T[K] extends (...args: infer A) => infer R
+  ? (...args: [...A, ack?: AckCallback]) => R
+    : T[K]
+}
